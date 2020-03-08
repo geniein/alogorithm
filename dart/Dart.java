69b6cc15462e01838d8fdb59291afa25d0a7e80e
ingenie <cubya@hotmail.co.kr>
2020-03-08 23:20:38 +0900

    Dart

---
+++ dart/Dart.java
@@ -0,0 +1,48 @@
+package dart;
+
+import java.util.*;
+
+public class Dart {
+ 
+    public static void main(String[] args) {
+        System.out.println(getPoint("1S2D*3T"));         
+    }
+
+    public static int getPoint(String pointStr) {
+        int[] arr = new int[3];
+        int curIdx = 0;
+        String tempNum = "";
+        for(char c : pointStr.toCharArray()) {
+            if(Character.isDigit(c)) {
+                tempNum = tempNum + c; 
+            }
+            else {
+                if(!"".equals(tempNum)) {
+                    arr[curIdx++] = Integer.parseInt(tempNum);
+                    tempNum = "";
+                }
+                switch (c) {
+                    case 'S' :
+                        arr[curIdx-1] = (int) Math.pow(arr[curIdx-1], 1);
+                        break;
+                    case 'D' :
+                        arr[curIdx-1] = (int) Math.pow(arr[curIdx-1], 2);
+                        break;
+                    case 'T' :
+                        arr[curIdx-1] = (int) Math.pow(arr[curIdx-1], 3);
+                        break;
+                    case '*' :
+                        arr[curIdx-1] = arr[curIdx-1]*2;
+                        if(curIdx-2 >= 0) {
+                            arr[curIdx-2] = arr[curIdx-2]*2;
+                        }
+                        break;
+                    case '#' :
+                        arr[curIdx-1] = arr[curIdx-1]*-1;
+                        break;
+                }
+            }
+        }
+        return arr[0]+arr[1]+arr[2];
+    }
+}
